7298e9464ff272ce909a204e69b2c0f4988c925e
2k0v11 <99639023+2k0v11@users.noreply.github.com>
2023-05-01 13:06:44 +0530

    portfolio

---
+++ src/components/About/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <Image
+            src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+          <div className="AboutBio">
+            Hello! My name is <strong>Kavya Kartik</strong>and As a Data Science student, I am thrilled to be part of a rapidly growing and dynamic field that is changing the way we live and work. Data science involves using mathematical, statistical, and computational techniques to extract insights and knowledge from vast amounts of data. In this program, I am learning how to analyze data, build predictive models, and generate valuable insights that can be used to drive business decisions, identify patterns and trends, and solve complex problems.
+          </div>
+          <div className="AboutBio tagline2">
+            I have become confident using the following technologies.
+          </div>
+          <Technologies>
+            {stackList.map((stack, index) => (
+              <Tech key={index} className="tech">
+                <TechImg src={stack.img} alt={stack.name} />
+                <TechName>{stack.name}</TechName>
+              </Tech>
+            ))}
+          </Technologies>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,51 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://media.tenor.com/OjxR-mgQmNoAAAAi/vutura-chatbot.gif",
+    title: "VIKBOT",
+    description: "University chatbot, prepared to address general querries of students and parents.",
+    tech_stack: "Python,React",
+    github_url: "https://github.com/2k0v11/University_Chatbot",
+    demo_url: "https://drive.google.com/file/d/1Ely1V2kaF3BulDNpK-43_N8oEnuASq-b/view?usp=share_link",
+  },
+
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Python-Light.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Java-Light.svg",
+    name: "Java",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg",
+    name: "Pandas",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/3/34/Microsoft_Office_Excel_%282019%E2%80%93present%29.svg",
+    name: "Excel",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/1024px-R_logo.svg.png?20160212050515",
+    name: "R",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Github-Dark.svg",
+    name: "Github",
+  },
+ 
+ 
+];
