03305f190bfec2a57f1e25917f90277978ff9bab
2k0v11 <99639023+2k0v11@users.noreply.github.com>
2023-05-05 12:15:51 +0530

    portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,7 +19,7 @@ function About() {
             alt="man-svgrepo"
           />
           <div className="AboutBio">
-            Hello! My name is <strong>Kavya Kartik</strong>and As a Data Science student, I am thrilled to be part of a rapidly growing and dynamic field that is changing the way we live and work. Data science involves using mathematical, statistical, and computational techniques to extract insights and knowledge from vast amounts of data. In this program, I am learning how to analyze data, build predictive models, and generate valuable insights that can be used to drive business decisions, identify patterns and trends, and solve complex problems.
+            Hello! My name is <strong>Kavya Kartik</strong> and As a Data Science student, I am thrilled to be part of a rapidly growing and dynamic field that is changing the way we live and work. Data science involves using mathematical, statistical, and computational techniques to extract insights and knowledge from vast amounts of data. In this program, I am learning how to analyze data, build predictive models, and generate valuable insights that can be used to drive business decisions, identify patterns and trends, and solve complex problems.
           </div>
           <div className="AboutBio tagline2">
             I have become confident using the following technologies.
