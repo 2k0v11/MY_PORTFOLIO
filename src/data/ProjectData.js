49ec5fd8a50a3a778ec528a64650a8525c98fe17
Kartik <kavyakartik6@gmail.com>
2024-05-10 14:32:56 +0530

    Update ProjectData.js

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,5 +1,12 @@
 // --------------------------------------- Project List
 export const ProjectList = [
+    {
+    img: "https://images.squarespace-cdn.com/content/v1/55ed989ee4b0c7f115ddc924/1541600620919-VEI2IOYGNT2WJXA2W4A0/analytics.gif",
+    title: "ANALYTICS & DASHBOARDS",
+    description: "Analytics projects with Python, Excel & Power BI",
+    tech_stack: "Jupyter Notebook/Colab, Excel, Power BI",
+    github_url: "https://github.com/2k0v11/Analysis-and-Dashboards", 
+  },
   {
     img: "https://media.tenor.com/OjxR-mgQmNoAAAAi/vutura-chatbot.gif",
     title: "CHATBOTS",
@@ -8,13 +15,30 @@ export const ProjectList = [
     github_url: "https://github.com/2k0v11/Chatbots",
     demo_url: "https://drive.google.com/file/d/1Ely1V2kaF3BulDNpK-43_N8oEnuASq-b/view?usp=share_link",
   },
+
   {
-    img: "https://1.bp.blogspot.com/-L2fdbFv6Kjk/WnorUC5l1mI/AAAAAAAAACQ/euAUIWWz3xw2MJ1UlcuyCSJzbLU65xq0gCLcBGAs/s640/2018-02-07_3-53-26.gif",
-    title: "Automated testing",
-    description: "Automated testing performed for various test cases including Facebook, Google, and Educational platform (MyCamu).",
-    tech_stack: "Java, Selenium",
-    github_url: "https://github.com/2k0v11/Automated_Testing/tree/main/project/src/session_1",
-    demo_url: "https://github.com/2k0v11/Automated_Testing/tree/main/project/src/outputs",
+    img: "https://cdn.dribbble.com/users/2440104/screenshots/6254368/machine-learning-_1_.gif",
+    title: "Machine Learning",
+    description: "List of ML projects covering various algorithms.",
+    tech_stack: "Jupyter Notebook",
+    github_url: "https://github.com/2k0v11/ML_Projects", 
+    
+  },
+  {
+    img: "https://theintercept.imgix.net/wp-uploads/sites/1/2018/11/Intercept_Echo_v2-3.5MB-2-1542062294.gif",
+    title: "SPEECH-RECOGNITION",
+    description: "Text2Speech & Speech2Text projects.",
+    tech_stack: "Jupyter Notebook",
+    github_url: "https://github.com/2k0v11/Speech-Recognition", 
+    
+  },
+  {
+    img: "https://www.1datagroup.com/wp-content/uploads/2020/12/Python-Web-Scraping-animated.gif",
+    title: "WEB SCRAPPING",
+    description: "Extracting data from websites and storing them into CSV files",
+    tech_stack: "Jupyter Notebook/Colab",
+    github_url: "https://github.com/2k0v11/WebScrapping", 
+   
   },
   {
     img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3g4eGE3Z2g1NHJkbnN0ODZjeGl4eWwxcnJ6c2Q2bDkzcjBheTZrOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Gev4knSOnIbKkGGET7/giphy.gif",
@@ -24,53 +48,30 @@ export const ProjectList = [
     github_url: "https://github.com/2k0v11/restaurant",
     demo_url: "https://restaurant-lnf59h6ph-kvk.vercel.app/?vercelToolbarCode=L8cMPrESz4IfYwf",
   },
-  {
-    img: "https://cdn.dribbble.com/users/2440104/screenshots/6254368/machine-learning-_1_.gif",
-    title: "Machine Learning",
-    description: "List of ML projects covering various algorithms.",
-    tech_stack: "Jupyter Notebook",
-    github_url: "https://github.com/2k0v11/ML_Projects", 
-    demo_url: "https://github.com/2k0v11/ML_Projects",
-  },
-  {
-    img: "https://theintercept.imgix.net/wp-uploads/sites/1/2018/11/Intercept_Echo_v2-3.5MB-2-1542062294.gif",
-    title: "SPEECH-RECOGNITION",
-    description: "Text2Speech & Speech2Text projects.",
-    tech_stack: "Jupyter Notebook",
-    github_url: "https://github.com/2k0v11/Speech-Recognition", 
-    demo_url: "https://github.com/2k0v11/Speech-Recognition",
-  },
-  {
-    img: "https://www.1datagroup.com/wp-content/uploads/2020/12/Python-Web-Scraping-animated.gif",
-    title: "WEB SCRAPPING",
-    description: "Extracting data from websites and storing them into CSV files",
-    tech_stack: "Jupyter Notebook/Colab",
-    github_url: "https://github.com/2k0v11/WebScrapping", 
-    demo_url: "https://github.com/2k0v11/WebScrapping",
-  },
    {
-    img: "https://images.squarespace-cdn.com/content/v1/55ed989ee4b0c7f115ddc924/1541600620919-VEI2IOYGNT2WJXA2W4A0/analytics.gif",
-    title: "ANALYTICS & DASHBOARDS",
-    description: "Analytics projects with Python, Excel & Power BI",
-    tech_stack: "Jupyter Notebook/Colab, Excel, Power BI",
-    github_url: "https://github.com/2k0v11/Analysis-and-Dashboards", 
-    demo_url: "https://github.com/2k0v11/Analysis-and-Dashboards",
+    img: "https://1.bp.blogspot.com/-L2fdbFv6Kjk/WnorUC5l1mI/AAAAAAAAACQ/euAUIWWz3xw2MJ1UlcuyCSJzbLU65xq0gCLcBGAs/s640/2018-02-07_3-53-26.gif",
+    title: "Automated testing",
+    description: "Automated testing performed for various test cases including Facebook, Google, and Educational platform (MyCamu).",
+    tech_stack: "Java, Selenium",
+    github_url: "https://github.com/2k0v11/Automated_Testing/tree/main/project/src/session_1",
+    demo_url: "https://github.com/2k0v11/Automated_Testing/tree/main/project/src/outputs",
   }
+
 ];
 
 // --------------------------------------- Skills
 
 export const stackList = [
   {
-    img: "https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Python-Light.svg",
+    img: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
     name: "Python",
   },
   {
-    img: "https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Java-Light.svg",
+    img: "https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white",
     name: "Java",
   },
   {
-    img: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg",
+    img: "https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white",
     name: "Pandas",
   },
   {
@@ -78,12 +79,12 @@ export const stackList = [
     name: "Excel",
   },
   {
-    img: "https://github.com/tandpfun/skill-icons/blob/main/icons/MySQL-Dark.svg",
+    img: "https://img.shields.io/badge/mysql-%2300000f.svg?style=for-the-badge&logo=mysql&logoColor=white",
     name: "SQL",
   },
   
   {
-    img: "https://github.com/tandpfun/skill-icons/blob/main/icons/R-Dark.svg",
+    img: "https://img.shields.io/badge/r-%23276DC3.svg?style=for-the-badge&logo=r&logoColor=white",
     name: "R",
   },
   {
@@ -91,7 +92,7 @@ export const stackList = [
     name: "HTML",
   },
   {
-    img: "https://github.com/microsoft/PowerBI-Icons/blob/main/SVG/Power-BI.svg",
+    img: "https://img.shields.io/badge/power_bi-F2C811?style=for-the-badge&logo=powerbi&logoColor=black",
     name: "POWER BI",
   },
  
